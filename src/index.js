3fae6d40ea963701281150d0099fd9004f777c37
github username <github email address>
2018-08-15 17:12:57 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-function handleSubmit(e) {
-
-}
-
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("identicon-form")
-  form.addEventListener("submit", handleSubmit)
+  const input = document.querySelector('#input')
+  let hashedArr
+  form.addEventListener('submit', handleSubmit)
 })
+
+
+function handleSubmit(e) {
+  e.preventDefault()
+  hashedArr = md5.array(input.value)
+  return hashedArr
+}
+
+function setColor(){
+  return `rbg(${hashedArr[7]}, ${hashedArr[4]}, ${hashedArr[1]})`
+}
+
+function hasColor(number){
+  number%2 === 0 ? true : false
+}
+// style="background-color:rgb(212, 29, 140)"
